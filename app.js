dff438bad8564a84cd527200f432db4124c75659
sirfrancois <sirfrancois4@gmail.com>
2021-02-17 14:59:16 -0600

    DiceyBusiness

---
+++ app.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    var button = document.getElementById('roll')
+    if (button) {
+        button.addEventListener('click', function (event) {
+            class Die {
+                constructor() {
+                    this.value;
+                }
+                roll() {
+                    this.value = Math.floor(Math.random() * 7);
+                    console.log(this.value);
+                    var value1 = document.createTextNode(this.value);
+                    var div = document.createElement("div");
+                    div.appendChild(value1);
+                    document.body.appendChild(div);
+                    return this.value;
+
+                }
+            }
+            var die = new Die();
+            die.roll();
+        })
+    }
+}
